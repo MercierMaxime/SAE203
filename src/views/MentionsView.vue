7493781834305aa1d248e3b0554e1e736853053f
MercierMaxime <maxime.mercier@edu.univ-fcomte.fr>
2022-05-30 15:10:57 +0200

    Les belles mentions

--- src/views/MentionsView.vue
+++ src/views/MentionsView.vue
@@ -11,5 +11,116 @@ export default {
 <template>
   <Headerpart />
 
-  <p class="flex text-center text-3xl font-bold text-black">Mentions légales :</p>
+  <p class="flex pl-5 pt-5 font-bold text-black sm:text-3xl lg:text-5xl xl:text-7xl">Mentions légales :</p>
+
+  <p class="flex pl-5 pt-5 text-black sm:text-base lg:text-lg xl:text-xl">
+    En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux
+    utilisateurs du site internet https://www.VieuxComme9(0).fr l'identité des différents intervenants dans le cadre de sa réalisation et de
+    son suivi:
+  </p>
+
+  <p class="flex pl-5 pt-5 text-black sm:text-base lg:text-lg xl:text-xl">
+    Propriétaire du site : Mercier Maxime 9 Impasse de la Fontaine 25600 Vieux-Charmont, France - Contact : maximemercier163@gmail.com
+    0659395230 - Adresse : 9 Impasse De La Fontaine 25600 Vieux-Charmont, France.
+  </p>
+
+  <p class="flex pl-5 pt-5 text-black sm:text-base lg:text-lg xl:text-xl">
+    Identification de l'entreprise : RGPD Mercier Maxime 9 Impasse de la Fontaine 25600 Vieux-Charmont, France au capital social de € -
+    SIREN : - RCS ou RM : - Adresse postale : 9 Impasse De La Fontaine 25600 Vieux-Charmont, France - [Consignes : ajoutez ici le lien
+    hypertexte vers la page de vos conditions générales de vente si vous en avez une.
+  </p>
+
+  <p class="flex pl-5 pt-5 text-black sm:text-base lg:text-lg xl:text-xl">Directeur de la publication : Maxime Mercier - Contact.</p>
+
+  <p class="flex pl-5 pt-5 text-black sm:text-base lg:text-lg xl:text-xl">
+    Hébergeur : Infomaniak - Rue Eugène-Marziano 25, 1227 Genève, Suisse - Téléphone : +41.22.820.35.44
+  </p>
+
+  <p class="flex pl-5 pt-5 text-black sm:text-base lg:text-lg xl:text-xl">
+    Délégué à la protection des données : Maxime Mercier - maximemercier163@gmail.com
+  </p>
+
+  <p class="flex pl-5 pt-5 text-black sm:text-base lg:text-lg xl:text-xl">
+    Mercier Maxime est propriétaire des droits de propriété intellectuelle et détient les droits d’usage sur tous les éléments accessibles
+    sur le site internet, notamment les textes, images, graphismes, logos, vidéos, architecture, icônes et sons.
+  </p>
+
+  <p class="flex pl-5 pt-5 text-black sm:text-base lg:text-lg xl:text-xl">
+    Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen
+    ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de Mercier Maxime 9 Impasse de la Fontaine 25600
+    Vieux-Charmont, France.
+  </p>
+
+  <p class="flex pl-5 pt-5 text-black sm:text-base lg:text-lg xl:text-xl">
+    Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient sera considérée comme constitutive d’une
+    contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.
+  </p>
+
+  <p class="flex pl-5 pt-5 text-black sm:text-base lg:text-lg xl:text-xl">
+    Mercier Maxime ne pourra être tenu pour responsable des dommages directs et indirects causés au matériel de l’utilisateur, lors de
+    l’accès au site https://www.VieuxComme9(0).fr.
+  </p>
+
+  <p class="flex pl-5 pt-5 text-black sm:text-base lg:text-lg xl:text-xl">
+    Mercier Maxime décline toute responsabilité quant à l’utilisation qui pourrait être faite des informations et contenus présents sur
+    https://www.VieuxComme9(0).fr.
+  </p>
+
+  <p class="flex pl-5 pt-5 text-black sm:text-base lg:text-lg xl:text-xl">
+    Mercier Maxime s’engage à sécuriser au mieux le site https://www.VieuxComme9(0).fr, cependant sa responsabilité ne pourra être mise en
+    cause si des données indésirables sont importées et installées sur son site à son insu.
+  </p>
+
+  <p class="flex pl-5 pt-5 text-black sm:text-base lg:text-lg xl:text-xl">
+    Des espaces interactifs (espace contact ou commentaires) sont à la disposition des utilisateurs. Mercier Maxime se réserve le droit de
+    supprimer, sans mise en demeure préalable, tout contenu déposé dans cet espace qui contreviendrait à la législation applicable en
+    France, en particulier aux dispositions relatives à la protection des données.
+  </p>
+
+  <p class="flex pl-5 pt-5 text-black sm:text-base lg:text-lg xl:text-xl">
+    Le cas échéant, Mercier Maxime se réserve également la possibilité de mettre en cause la responsabilité civile et/ou pénale de
+    l’utilisateur, notamment en cas de message à caractère raciste, injurieux, diffamant, ou pornographique, quel que soit le support
+    utilisé (texte, photographie …).
+  </p>
+
+  <p class="flex pl-5 pt-5 text-black sm:text-base lg:text-lg xl:text-xl">
+    Conformément aux dispositions de la loi 78-17 du 6 janvier 1978 modifiée, l’utilisateur du site https://www.VieuxComme9(0).fr dispose
+    d’un droit d’accès, de modification et de suppression des informations collectées. Pour exercer ce droit, envoyez un message à notre
+    Délégué à la Protection des Données : Maxime Mercier - maximemercier163@gmail.com.
+  </p>
+
+  <p class="flex pl-5 pt-5 text-black sm:text-base lg:text-lg xl:text-xl">
+    Pour plus d'informations sur la façon dont nous traitons vos données (type de données, finalité, destinataire...), lisez notre
+    https://www.VieuxComme9(0).fr/mentions-legales.
+  </p>
+
+  <p class="flex pl-5 pt-5 text-black sm:text-base lg:text-lg xl:text-xl">
+    Le site https://www.VieuxComme9(0).fr contient des liens hypertextes vers d’autres sites et dégage toute responsabilité à propos de ces
+    liens externes ou des liens créés par d’autres sites vers https://www.VieuxComme9(0).fr.
+  </p>
+
+  <p class="flex pl-5 pt-5 text-black sm:text-base lg:text-lg xl:text-xl">
+    Un "cookie" est un fichier de petite taille qui enregistre des informations relatives à la navigation d’un utilisateur sur un site. Les
+    données ainsi obtenues permettent d'obtenir des mesures de fréquentation, par exemple.
+  </p>
+
+  <p class="flex pl-5 pt-5 text-black sm:text-base lg:text-lg xl:text-xl">
+    Vous avez la possibilité d’accepter ou de refuser les cookies en modifiant les paramètres de votre navigateur. Aucun cookie ne sera
+    déposé sans votre consentement.
+  </p>
+
+  <p class="flex pl-5 pt-5 text-black sm:text-base lg:text-lg xl:text-xl">
+    Les cookies sont enregistrés pour une durée maximale de 3 mois.
+  </p>
+
+  <p class="flex pl-5 pt-5 text-black sm:text-base lg:text-lg xl:text-xl">
+    Pour plus d'informations sur la façon dont nous faisons usage des cookies, lisez notre https://www.VieuxComme9(0).fr/mentions-legales.
+  </p>
+
+  <p class="flex pl-5 pt-5 pb-10 text-black sm:text-base lg:text-lg xl:text-xl">
+    Tout litige en relation avec l’utilisation du site https://www.VieuxComme9(0).fr est soumis au droit français. En dehors des cas où la
+    loi ne le permet pas, il est fait attribution exclusive de juridiction aux tribunaux compétents de Montbéliard.
+  </p>
+
+  <Footerpart />
 </template>
